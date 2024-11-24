80f4a727c411d3b26d16ac6a57e13a8f35eab4b8
Xian20060128 <4B3G0050@office.stust.edu.tw>
2024-11-24 21:56:52 +0800

    0

---
+++ 2_class_1/Person.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Person
+{
+private:
+	string id;
+	string lastName;
+	string firstName;
+	string gender;
+	string birthDate;
+
+public:
+	// Constructor, ???c?l, ???c????
+	Person(const string& id, const string& lastName, const string& firstName, const string& gender, const string& birthDate);
+	Person();
+
+	// Getter, ????????
+	string getId() const;
+	string getLastName() const;
+	string getFirstName() const;
+	string getGender() const;
+	string getBirthDate() const;
+
+	// Setter, ?]??????
+	void setId(const string& id);
+	void setLastName(const string& lastName);
+	void setFirstName(const string& firstName);
+	void setGender(const string& gender);
+	void setBirthDate(const string& birthDate);
+
+	void display() const;
+};
+
