c3716de6b1ee4d097b05b3abca88cf5f1e5d3afa
Xian20060128 <4B3G0050@office.stust.edu.tw>
2024-11-24 22:04:57 +0800

    0

--- 2_class_1/Person.h
+++ 2_class_1/Person.h
@@ -5,31 +5,25 @@ using namespace std;
 class Person
 {
 private:
-	string id;
-	string lastName;
-	string firstName;
-	string gender;
-	string birthDate;
-
+string id;
+string lastName;
+string firstName;
+string gender;
+string birthDate;
 public:
-	// Constructor, ???c?l, ???c????
 	Person(const string& id, const string& lastName, const string& firstName, const string& gender, const string& birthDate);
 	Person();
 
-	// Getter, ????????
-	string getId() const;
-	string getLastName() const;
-	string getFirstName() const;
-	string getGender() const;
-	string getBirthDate() const;
-
-	// Setter, ?]??????
-	void setId(const string& id);
-	void setLastName(const string& lastName);
-	void setFirstName(const string& firstName);
-	void setGender(const string& gender);
-	void setBirthDate(const string& birthDate);
-
-	void display() const;
+       string getId() const;
+	   string getLastName() const;
+	   string getFirstName() const;
+	   string getGender() const;
+	   string getBirthDate() const;
+	   void setId(const string& id);
+	   void setLastName(const string& lastName);
+	   void setFirstName(const string& firstName);
+	   void setGender(const string& gender);
+	   void setBirthDate(const string& birthDate);
+	   void display() const;
 };
 
