96259c1dfcba3f906d36f8c21914fe9395b622b1
Xian20060128 <4B3G0050@office.stust.edu.tw>
2024-11-24 22:11:45 +0800

    0

--- 2_class_1/2_class_1.cpp
+++ 2_class_1/2_class_1.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 int main()
 {
-   Person person1("A123456789", "Chen", "Jason", "M", "1999-01-01");
+   Person person1("L568465954", "Chen", "Oscar", "M", "2005-01-28");
    Person* person2 = new Person();
-   person2->setId("B987654321");
-   person2->setLastName("Wang");
-   person2->setFirstName("David");
-   person2->setGender("M");
-   person2->setBirthDate("1999-12-31");
+   person2->setId("L987654321");
+   person2->setLastName("Lin");
+   person2->setFirstName("Candy");
+   person2->setGender("F");
+   person2->setBirthDate("2005-12-02");
    person1.display();
    cout << endl;
    person2->display();
